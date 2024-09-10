0e08fc1553b56188c5513b2ceb8af500009120f3
sohail019 <shaikhhsohail0193@gmail.com>
2024-09-10 17:20:39 +0530

    Added speech recognition functionality

--- src/components/Main/SearchBox.jsx
+++ src/components/Main/SearchBox.jsx
@@ -1,8 +1,45 @@
 import { LuImagePlus } from "react-icons/lu";
 import { FaMicrophone } from "react-icons/fa";
 import { IoMdSend } from "react-icons/io";
+import { useState } from "react";
 
 export const SearchBox = ({ input, setInput, onSent }) => {
+  
+  const [isListening, setIsListening] = useState(false);
+
+  //* Function to start listening using Web Speech API
+  const startListening = () => {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.lang = "en-US";
+    recognition.interimResults = false //? don't return partial results
+
+    recognition.onstart = () => {
+      setIsListening(true)
+      console.log("Listening for Speech...")
+    }
+
+    recognition.onresult = (event) => {
+      const transcript = event.results[0][0].transcript
+      console.log("Recognized", transcript)
+      setInput(transcript) //? set the input to the recognized text
+      onSent(transcript)
+      setIsListening(false) //? stop the listening state
+    }
+
+    recognition.onerror = (event) => {
+      console.error("Speech Recognition Error:", event.error);
+      setIsListening(false) //? stop listening state in case of error
+    }
+
+    recognition.onend = () => {
+      console.log("Speech Recognition Ended");
+      setIsListening(false)
+    }
+
+    recognition.start()
+  }
   return (
     <div className="search-box">
       <input
@@ -16,7 +53,8 @@ export const SearchBox = ({ input, setInput, onSent }) => {
       />
       <div>
         <LuImagePlus className="icon" />
-        <FaMicrophone className="icon" />
+        {/* Speech */}
+        <FaMicrophone className={`icon ${isListening ? "listening" : ""}`} onClick={startListening} />
         {input && (
           <IoMdSend className="icon" onClick={() => onSent()} />
         )}
